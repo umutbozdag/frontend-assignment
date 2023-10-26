5cd35a0bc68a2dab92f4ae437118670c6deb5397
umut <umutbozdag@outlook.com.tr>
2023-10-26 17:21:35 +0300

    refactor: refactor Hero

--- src/layout/hero/Hero.tsx
+++ src/layout/hero/Hero.tsx
@@ -1,15 +1,30 @@
 import classnames from 'classnames/bind';
-import Features from '../../components/Features/Features';
-import Tabs from '../../components/Tabs/Tabs';
+import Tabs from '../../components/Tabs';
 import { tabs } from '../../config/tabs';
 import styles from './Hero.module.scss';
+import { Context } from '../../context/ContextProvider';
+import { useContext } from 'react';
+import { features } from '../../config/features';
+import FeatureItem from '../../components/FeatureItem';
 
 const cx = classnames.bind(styles);
 
 export const Hero = () => {
+  const {
+    sharedData: { activeTab },
+  } = useContext(Context)!;
+  const currentFeature = features[activeTab];
   return (
-    <section className={cx('hero')}>
-      <Features />
+    <section className={cx('heroSection')}>
+      <div className={cx('featureWrapper')}>
+        <FeatureItem
+          key={currentFeature.id}
+          title={currentFeature.title}
+          description={currentFeature.description}
+          subtitle={currentFeature.subtitle}
+          frame={currentFeature.frame}
+        />
+      </div>
       <Tabs tabs={tabs} />
     </section>
   );
